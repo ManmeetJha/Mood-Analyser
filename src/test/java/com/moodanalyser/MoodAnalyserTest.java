421e46f22c2231691f79759db0d744babbc88b48
Navneet <navneetjh5@gmail.com>
2020-10-09 02:53:45 +0530

    MoodAnalyser Class and Test Class created

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -1,2 +1,24 @@
-package com.moodanalyser;public class MoodAnalyserTest {
+package com.moodanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+
+    @Test
+    public void givenMessage_whenSad_ShouldreturSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad Message");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMessage_whenNotSad_ShouldreturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY",mood);
+    }
+    
 }
+
